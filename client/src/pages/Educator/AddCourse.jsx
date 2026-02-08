f5f4c1d78e01cc08a4a2c466cc6db1cbef1f4875
Suryansh <shubamsharma44122@gmail.com>
2026-02-08 23:27:06 +0530

    Add frontend for add course page

--- client/src/pages/Educator/AddCourse.jsx
+++ client/src/pages/Educator/AddCourse.jsx
@@ -33,11 +33,118 @@ const AddCourse = () => {
     }
   },[])
 
+  const handleChapter=(action,chapterId)=>{
+    if(action==='add'){
+      const title=prompt('Enter chapter name:');
+      if(title){
+        const newChapter={
+          chapterId:uniqid(),
+          chapterTitle:title,
+          chapterContent:[],
+          collapsed:false,
+          chapterOrder:chapters.length>0 
+          ?
+           chapters.slice(-1)[0].chapterorder+1
+           :
+           1,
+        };
+        setChapters([...chapters,newChapter]);//here ... in chapters is used so that 
+                                              //previous data inside the chapter  doent 
+                                              // get replaces with new chapter value
+}
+      }
+      else if(action==='remove'){
+        setChapters(chapters.filter((chapter)=>
+          chapter.chapterId!==chapterId));
+      }
+      else if(action==='toggle'){
+        setChapters(
+          chapters.map((chapter)=>chapter.chapterId===chapterId
+          ?
+          {...chapter,collapsed:!chapter.collapsed}
+          :
+          chapter
+        )
+        )
+      }
+    }
+    
+
+ //add Lectures
+// This function adds a new lecture to the currently selected chapter.
+// It loops through all chapters using map() and finds the chapter whose id
+// matches currentChapterId. Once found, it creates a new lecture object using
+// lectureDetails and assigns a lectureOrder.
+// If the chapter already has lectures, order = last lecture order + 1,
+// otherwise order starts from 1.
+// Then the new lecture is pushed into that chapter's chapterContent array
+// and the updated chapters state is saved using setChapters().
+    const addLecture=()=>{
+      setChapters(
+        chapters.map((chapter)=>{
+          if(chapter.chapterId===currentChapterId){
+            const newLecture={
+              ...lectureDetails,
+              lectureOrder:chapter.chapterContent.length>0 
+              ?
+              chapters.slice(-1)[0].lecturerOrder+1
+              :
+              1,
+            };
+            chapter.chapterContent.push(newLecture)
+          }
+          return chapter;
+        })
+      );
+      setShowPopup(false);
+      setLectureDetails({
+        lecturTitle:'',
+        lectureDuration:'',
+        lectureUrl:'',
+        isPreviewFree:false,
+      })
+    }
+  // This function handles lecture actions like adding or removing a lecture.
+// It receives three parameters:
+// action → tells what to do ("add" or "remove")
+// chapterId → identifies which chapter to modify
+// lectureIndex → tells which lecture to remove from that chapter
+
+// If action is "add":
+// It sets the current chapter id and opens the popup form to add a new lecture.
+
+// If action is "remove":
+// It loops through all chapters using map(), finds the matching chapter using chapterId,
+// and removes the lecture at lectureIndex from that chapter's lecture array using splice().
+// Finally, it updates the chapters state with the modified data.
+    
+    const handleLecture=(action,chapterId,lectureIndex)=>{
+      if(action==='add'){
+        setCurrentChapterId(chapterId);
+        setShowPopup(true);
+      }
+      else if(action==='remove'){
+        setChapters(
+          chapters.map((chapter)=>{
+            if(chapter.chapterId===chapterId){
+              chapter.chapterContent.splice(lectureIndex,1);{/*splice() is used to remove 
+              // or add elements in array.Finds the correct chapter using chapterId
+👉            //Removes one lecture from that chapter using lectureIndex*/}
+            }
+          })
+        )
+      }
+    }
+   
+    const handleSubmit=async(e)=>{
+      e.preventDefault()
+    }
   return (
     <div className='h-screen overflow-scroll flex flex-col 
     items-start justify-between md:p-8 md:pb-0 p-4 pt-8 pb-0'>
     
-<form className='flex flex-col gap-6 max-w-3xl'>
+    <form className='flex flex-col 
+    gap-6 max-w-3xl' onSubmit={handleSubmit}>
 
   {/* Course Title */}
   <div className='flex flex-col gap-1'>
@@ -63,6 +170,8 @@ const AddCourse = () => {
       className='bg-white border border-gray-300 rounded min-h-[130px]'
     ></div>
     </div>
+    
+    {/*course proce ans course thumbnail upload field*/}
     <div className='flex items-center justify-between flex-wrap'>
       <div className='flex flex-col gap-1'>
         
@@ -74,6 +183,7 @@ const AddCourse = () => {
       border-gray-500' required/>
 
       </div>
+      {/** course thumbnail upload field*/}  
       <div className='flex md:flex-row flex-col items-center gap-3'>
         <p>Course Thumbnail</p>
         
@@ -92,6 +202,8 @@ const AddCourse = () => {
        </label>
       </div>
     </div>
+
+     {/**Discount % input field */}
     <div className='flex flex-col gap-1'>
       <p>Discount %</p>
       <input onChange={e=>setDiscount(e.target.value)}
@@ -99,6 +211,143 @@ const AddCourse = () => {
       max={100} className='outline-none md:py-2.5 py-2 w-28 px-3
       rounded border border-gray-500' requires/>
     </div>
+
+     {/*Adding chapters and lectures*/}
+     <div>
+      {chapters.map((chapter,chapterIndex)=>(
+       <div key={chapterIndex} className='
+       bg-white border rounded-lg mb-4 '>
+
+        <div className='flex justify-between 
+        items-center p-4 border-b'>
+        
+        <div className='flex items-center'>
+
+         <img src={assets.dropdown_icon}
+          width={14} alt="" className={`mr-2 
+          cursor-pointer transition-all 
+          ${chapter.collapsed && "-rotate-90"}`}
+          onClick={()=>handleChapter('toggle',
+          chapter.chapterId)}/>
+
+          <span className='font-semibold'>
+          {chapterIndex+1} {chapter.chapterTitle}
+          </span>
+         </div>
+
+         <span className='text-gray-500'>
+         {chapter.chapterContent.length} Lectures</span>
+          
+          <img src={assets.cross_icon} alt="" 
+          className='cursor-pointer' onClick={()=>
+          handleChapter('remove',chapter.chapterId)}/>
+
+           </div>
+           
+           {!chapter.collapsed && (
+            <div className='p-4'>
+              
+              {chapter.chapterContent.map((lecture,lectureIndex)=>(
+                
+                <div key={lectureIndex} className='flex 
+                justify-between items-center mb-2'>
+
+                  <span>{lectureIndex+1} {lecture.lectureTitle}-
+                   {lecture.lectureDuration} mins - <a href=
+                   {lecture.lectureUrl} target="_blank" className='
+                   text-blue-500'>Link</a>-{lecture.isPreviewFree ? 
+                   'Free Preview' :'Paid'}</span>
+                    
+                    <img src={assets.cross_icon} alt=""
+                    className='cursor-pointer' onClick=
+                    {()=>handleLecture('remove',chapter.chapterId)}/>
+
+                  </div>
+              ))}
+
+              <div className='inline-flex bg-gray-100 p-2 
+              rounded cursor-pointer mt-2' onClick={()=>
+              handleLecture('add',chapter.chapterId)}>
+                + Add Lecture</div>
+
+              </div>
+           )}
+        </div>
+        ))}
+        <div className='flex justify-center items-center 
+        bg-blue-100 p-2 rounded-lg cursor-pointer' onClick=
+        {()=>handleChapter('add')}>+ Add Chapter</div>
+
+        {showPopup&& (
+          <div className='fixed inset-0 flex items-center 
+          justify-center bg-gray-800 bg-opacity-50'>
+
+          <div className='bg-white text-gray-700 
+          p-4 rounded relative w-full max-w-80'>
+
+            <h2 className='text-lg font-semibold 
+            mb-4'>Add Lecture</h2>
+            
+            <div className='mb-2'>
+
+            <p>Lecture Title</p>
+
+            <input type='text'
+            className='mt-1 block w-full border rounded py-1 px-2'
+            value={lectureDetails.lectureTitle}
+            onChange={(e)=>setLectureDetails
+            ({...lectureDetails,lectureTitle:e.target.value})}/>
+           
+            </div>
+
+             <div className='mb-2'>
+
+            <p>Lecture Duration</p>
+
+            <input type='number'
+            className='mt-1 block w-full border rounded py-1 px-2'
+            value={lectureDetails.lectureDuration}
+            onChange={(e)=>setLectureDetails
+            ({...lectureDetails,lectureDuration:e.target.value})}/>
+           
+            </div>
+
+             <div className='mb-2'>
+
+            <p>Lecture URL</p>
+
+            <input type='text'
+            className='mt-1 block w-full border rounded py-1 px-2'
+            value={lectureDetails.lectureUrl}
+            onChange={(e)=>setLectureDetails
+            ({...lectureDetails,lectureUrl:e.target.value})}/>
+           
+            </div>
+
+             <div className='mb-2 flex gap-2'>
+
+            <p>Is preview Free?</p>
+
+            <input type="checkbox"
+            className='mt-1 scale-125'
+            checked={lectureDetails.isPreviewFree}
+            onChange={(e)=>setLectureDetails
+            ({...lectureDetails,isPreviewFree:e.target.checked})}/>
+           
+            </div>
+
+            <button type='button' className='w-full bg-blue-400 
+            text-white px-4 py-2 rounded' onClick={addLecture}>Add</button>
+
+            <img onClick={()=>setShowPopup(false)} src={assets.cross_icon}
+            className='absolute top-4 right-4 w-4 cursor-pointer' alt=""/>
+
+            </div>
+            </div>
+        )}
+     </div>
+    <button type='submit' className='bg-black text-white
+     w-max py-2.5 px-8 rounded my-4'>ADD</button>
 
 
 
